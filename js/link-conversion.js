387a3593b9599a6df9638e13e2da39d7d258ef48
padowla <62257411+padowla@users.noreply.github.com>
2026-01-16 11:31:17 +0100

    Update link-conversion.js

--- js/link-conversion.js
+++ js/link-conversion.js
@@ -1,13 +1,13 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function ()
-{
+document.addEventListener('DOMContentLoaded', function () {
 	const a = document.getElementById('link-conversion');
 
-	a.setAttribute('href', a.getAttribute('href')
-		.replace('-', 'ob@')
-		.replaceAll('-', '.')
-		.replace('/', 'samuelepadula.it')
-		.replace('to', 'mailto:j')
+	a.setAttribute(
+		'href',
+		a.getAttribute('href')
+			.replace('to-', 'mailto:jo')
+			.replace('jbba', 'b@')
+			.replace('-it/', 'samuelepadula.it')
 	);
 });
